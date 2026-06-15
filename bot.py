2c99df4c8048bfa4b00f64d478026ce3341722d5
gurubot77 <152202886+gurubot77@users.noreply.github.com>
2026-06-15 18:20:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,6 +54,13 @@ async def delete_later(context, chat_id, msg_id):
         pass
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    WELCOME_IMAGE = "https://i.imgur.com/Rxk3TQL.jpeg"
+
+await context.bot.send_photo(
+    chat_id=update.effective_chat.id,
+    photo=WELCOME_IMAGE,
+    caption="🔥 Welcome to TMBD BOT 🔥"
+)
     if context.args and context.args[0].startswith("batch_"):
         batch_id = context.args[0].replace("batch_", "")
 
