9a63b8e2bf5d7668174776e9c45fb84bb68a4c80
gurubot77 <152202886+gurubot77@users.noreply.github.com>
2026-06-15 12:46:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, BotComm
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("BOT_TOKEN")
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+BOT_USERNAME = os.getenv("TMBD0_BOT")
 BOT_USERNAME = "TMBD0_BOT"
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
